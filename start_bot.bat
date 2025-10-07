a4d60b0ec08e295303b52ca6576df70006d5a919
Mohammad Aghdam <mohammad.m.aghdam@gmail.com>
2025-10-07 09:08:04 +0200

    created start_bot file

---
+++ start_bot.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Starting the bot in LIVE mode...
+"C:\Users\moham\miniconda3\envs\drl\python.exe" "g:\My Drive\Github\Deep-Reinforcement-Learning-MT5-Bot\live_bot.py" --live
+echo.
+echo The bot has finished or was closed.
+pause
